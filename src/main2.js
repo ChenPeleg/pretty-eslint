f7989e6136fb67a70ce774b9442cb3f836b9f1ce
chenpeleg <chenpeleg@gmail.com>
2022-09-21 14:32:12 +1000

    auto formats on save

--- src/main2.js
+++ src/main2.js
@@ -1,6 +1,10 @@
-class mainClass {
+class mainClass2 {
     constructor() {
         this.data = [{ a: 2 }];
         const a = 2;
+    }
+    anotherMethod() {
+        let a;
+        let b;
     }
 }
