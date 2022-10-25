46e8f63d636e377452f7144397a47c3d7f167214
chenp <chenpeleg@gmail.com>
2022-10-25 10:51:48 +0300

    added another command to lint

--- tests/main.js
+++ tests/main.js
@@ -1,8 +1,7 @@
 class mainClass {
     constructor() {
         var singleQuote;
-        this.data = [{ a: 2 }];
-
+        this.data = [{ a: 2      }]
         const a = 2;
     }
     /** @type{(() =>void)} */
